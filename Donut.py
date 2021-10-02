cad31d15142242968ce26aaa0c23278afbf40c60
AbaddonGamer <64578820+AbaddonGamer@users.noreply.github.com>
2021-10-02 13:10:44 +0530

    Create Donut.py

---
+++ Donut.py
@@ -0,0 +1,5 @@
+from vpython import *
+canvas(background=color.black)
+donut =ring(radius=0.5,thickness=0.25,color=vector(400,100,1))
+chocolate =ring(radius=0.55,thickness=0.25,color=vector(0.4,0.2,0))
+
